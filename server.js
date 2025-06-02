7376babaa1ad5b052c12f499b5fbff54d56ae0e7
Arun Kumar <arunku9050@gmail.com>
2025-06-02 21:42:14 +0530

    Edited code to fix bug / add feature

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const express= require("express");
-const bodyparser=require('body-parser');
-const mongoose=require('mongoose');
+const express = require("express");
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
 
-const  app=express();
+const app = express();
 
-app.set("view engine","ejs");
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect("mongodb+srv://arunku9050:zsgXK66fy4Hk75Nu@cluster0.jsl0dve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(()=>console.log("MOngoDB CONNECTED"))
-    .catch(err=> console.log(err));
+mongoose.connect("mongodb://127.0.0.1:27017/todo")
+    .then(() => console.log("MongoDB CONNECTED"))
+    .catch(err => console.log(err));
 
-
-const tryschema =new mongoose.Schema({
-    name:String
+// Schema and model
+const tryschema = new mongoose.Schema({
+    name: String
 });
-const item=mongoose.model("task",tryschema);
+const item = mongoose.model("task", tryschema);
+
+// Optional: Insert default items (only run once)
 const defaultItems = [
     { name: "some video" },
     { name: "DSA" },
@@ -24,45 +26,47 @@ const defaultItems = [
     { name: "Nodejs" },
     { name: "some rest" }
 ];
-item.find({}).then(existing=>{
-    if(existing.length===0){
-    item.insertMany(defaultItems)
-    .then(() => console.log("Default items inserted"))
-    .catch(err => console.log(err));
+
+item.find({}).then(existing => {
+    if (existing.length === 0) {
+        item.insertMany(defaultItems)
+            .then(() => console.log("Default items inserted"))
+            .catch(err => console.log(err));
     }
 });
-app.get('/',async function(req,res){
-    try{
-    const foundItems=await item.find({});
-        res.render("list",{dayej:foundItems}); 
-    }catch(err){
-        console.log(err);
 
+// GET route
+app.get('/', async function (req, res) {
+    try {
+        const foundItems = await item.find({});
+        res.render("list", { dayej: foundItems });
+    } catch (err) {
+        console.log(err);
         res.status(500).send("Internal server error");
     }
-})
+});
 
-//post
 // POST route to add new item
 app.post('/', async function (req, res) {
-    const itemName = req.body.ele1;
-    if (itemName.trim() !== "") {
-        const newItem = new item({ name: itemName });
+    const newTask = req.body.ele1;
+    if (newTask.trim() !== "") {
+        const newItem = new item({ name: newTask });
         await newItem.save();
     }
     res.redirect('/');
 });
-//delete
+
 app.post('/delete', async function (req, res) {
-    const checked = req.body.checkbox1;
-    
-    try{
-        await item.findByIdAndDelete(checked);
-    }catch(err){
+    const checkedId = req.body.checkbox1;
+    try {
+        await item.findByIdAndDelete(checkedId);
+    } catch (err) {
         console.log(err);
     }
     res.redirect('/');
 });
-app.listen("5000",function(){
-    console.log("server is running");
+
+
+app.listen(5000, function () {
+    console.log("Server is running on port 5000");
 });
